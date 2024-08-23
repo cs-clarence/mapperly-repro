8e1bf3a515d2e0e0e20fdf4436b7f7d7f2e571c7
Clarence Manuel <rencema@cloudstaff.com>
2024-08-23 13:17:44 +0800

    initial commit

---
+++ PrivatePackage/User.cs
@@ -0,0 +1,6 @@
+﻿namespace PrivatePackage;
+
+public class User(string emailAddress)
+{
+    public string EmailAddress { get; private set; } = emailAddress;
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using PrivatePackage;
+using Riok.Mapperly.Abstractions;
+
+public record UserUpdateDto(string EmailAddress);
+
+[Mapper(IncludedMembers = MemberVisibility.All)]
+public static partial class UserMapper 
+{
+	static partial void ApplyTo(this UserUpdateDto input, User user);
+	
+	public static void ApplyUpdates(this UserUpdateDto input, User user) 
+	{
+		input.ApplyTo(user);
+	}
+}
+					
+public class Program
+{
+	public static void Main()
+	{
+		var user = new User("user@gmail.com");
+		var updates = new UserUpdateDto("new-user@gmail.com");
+		
+		updates.ApplyUpdates(user);
+		
+		Console.WriteLine(user.EmailAddress); // still shows user@gmail.com
+	}
+}
